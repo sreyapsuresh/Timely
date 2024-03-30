e165dfd47cc77c94249905ec875ffaaa9b6e3b0c
sreyapsuresh <sreyapsuresh@gmail.com>
2024-03-29 22:57:43 -0500

    Initial Commit

---
+++ Timely/TimelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelyApp.swift
+//  Timely
+//
+//  Created by Sreya Suresh on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TimelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
